9bd7a6a6e595705e595eaef486bf6f4be6a5ca45
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-01-11 19:45:21 +0100

    Updated BotClient

--- ElGuayaBot.Application.Implementation/BotClient.cs
+++ ElGuayaBot.Application.Implementation/BotClient.cs
@@ -2,6 +2,7 @@ using System;
 using System.Net.Http;
 using ElGuayaBot.Application.Contracts;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MihaZupan.TelegramBotClients;
 using Telegram.Bot;
 
@@ -9,19 +10,23 @@ namespace ElGuayaBot.Application.Implementation
 {
     public class BotClient: IBotClient
     {
+        private ILogger<BotClient> _logger;
         public BlockingTelegramBotClient Client { get; }
  
-         public BotClient(IConfiguration configuration)
-         {
-             try
-             {
-                 Client = new BlockingTelegramBotClient(configuration["TelegramBotToken"], (HttpClient) null/*, new SchedulerSettings(60, 10, 500, 6, 1500, 6)*/);
-             }
-             catch (Exception e)
-             {
-                 Console.WriteLine(e);
-                 throw;
-             }
-         }
+        public BotClient(IConfiguration configuration, ILogger<BotClient> logger)
+        {
+            _logger = logger;
+            
+            try
+            {
+                Client = new BlockingTelegramBotClient(configuration["TelegramBotToken"], (HttpClient) null/*, new SchedulerSettings(60, 10, 500, 6, 1500, 6)*/);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Error setting TelegramBotClient", e);
+                
+                throw;
+            }
+        }
      }
  }
