f6d02086314484fdcc1d095bbef6c8ba41115ace
Zabrios <dinocren@gmail.com>
2019-01-19 19:51:29 +0100

    Picture Search Service's Interface and class. [NO IMPLEMENTATION YET]

---
+++ ElGuayaBot.Infrastructure.Contracts/Service/IPictureSearchService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElGuayaBot.Infrastructure.Contracts.Service
+{
+    public interface IPictureSearchService
+    {
+        IEnumerable<string> GetImageUrlList(string[] topics, int quantity = 50);
+
+        IEnumerable<string> GetGifUrlList(string[] topics, int quantity = 50);
+
+        string GetRandomImage(string[] topics);
+
+        string GetRandomGif(string[] topics);
+    }
+}
---
+++ ElGuayaBot.Infrastructure.Implementation/Service/PictureSearchService.cs
@@ -0,0 +1,30 @@
+﻿using ElGuayaBot.Infrastructure.Contracts.Service;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElGuayaBot.Infrastructure.Implementation.Service
+{
+    public class PictureSearchService : IPictureSearchService
+    {
+        public IEnumerable<string> GetGifUrlList(string[] topics, int quantity = 50)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetImageUrlList(string[] topics, int quantity = 50)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomGif(string[] topics)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetRandomImage(string[] topics)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
