a5c69c31148432a5b61a45db5f7d01b6606fc798
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-02 14:56:17 +0200

    Updated Chat and User model with ChatUser

--- ElGuayaBot.Domain.Entity/Chat.cs
+++ ElGuayaBot.Domain.Entity/Chat.cs
@@ -1,12 +1,20 @@
 using System;
+using System.Collections.Generic;
 
 namespace ElGuayaBot.Domain.Entity
 {
     public class Chat
     {
+        public Chat()
+        {
+            Users = new HashSet<ChatUser>();
+        }
+        
         public long Id { get; set; }
         public string Title { get; set; }
         public string Type { get; set; }
         public DateTime FirstSeen { get; set; }
+        
+        public ICollection<ChatUser> Users { get; set; }
     }
 }
--- ElGuayaBot.Domain.Entity/User.cs
+++ ElGuayaBot.Domain.Entity/User.cs
@@ -5,10 +5,17 @@ namespace ElGuayaBot.Domain.Entity
 {
     public class User
     {
+        public User()
+        {
+            Chats = new HashSet<ChatUser>();
+        }
+        
         public int Id { get; set; }
         public bool IsBot { get; set; }
         public string LanguageCode { get; set; }
         public string Username { get; set; }
         public DateTime FirstSeen { get; set; }
+        
+        public ICollection<ChatUser> Chats { get; set; }
     }
 }
