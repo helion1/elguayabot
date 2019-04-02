49fb40bb015e8d2f63814083e09291a42a2d7328
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-02 15:42:31 +0200

    Updated ChatUser

--- ElGuayaBot.Domain.Entity/ChatUser.cs
+++ ElGuayaBot.Domain.Entity/ChatUser.cs
@@ -2,6 +2,9 @@ namespace ElGuayaBot.Domain.Entity
 {
     public class ChatUser
     {
+        public long ChatId { get; set; }
+        public int UserId { get; set; }
+
         public Chat Chat { get; set; }
         public User User { get; set; }
     }
