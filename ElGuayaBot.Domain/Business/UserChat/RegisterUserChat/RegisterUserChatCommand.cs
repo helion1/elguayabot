033c3ea45c7d6af64affa2bd5b3519972d662926
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-03 12:10:46 +0200

    Moved things around

--- ElGuayaBot.Domain/Business/UserChat/RegisterUserChat/RegisterUserChatCommand.cs
+++ ElGuayaBot.Domain/Business/UserChat/RegisterUserChat/RegisterUserChatCommand.cs
@@ -1,9 +1,8 @@
-using System;
 using ElGuayaBot.Common.Request;
 using ElGuayaBot.Common.Result;
 using ElGuayaBot.Domain.Entity;
 
-namespace ElGuayaBot.Domain.Business.UserChat
+namespace ElGuayaBot.Domain.Business.UserChat.RegisterUserChat
 {
     public class RegisterUserChatCommand : Request<Result>
     {
