8aa9f6e1e54c1ae000680e90f5ef92acdbf83b7a
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-03 17:14:58 +0200

    Created UpdateNotification

---
+++ ElGuayaBot.Domain/Business/Updates/UpdateNotification.cs
@@ -0,0 +1,22 @@
+using ElGuayaBot.Domain.Entity;
+using MediatR;
+
+namespace ElGuayaBot.Domain.Business.Updates
+{
+    public class UpdateNotification : INotification
+    {
+        public int Id { get; set; }
+        public UpdateType Type { get; set; }
+        
+        public User[] NewChatMembers { get; set; }
+        public User LeftChatMember { get; set; }
+        public string NewChatTitle { get; set; }
+    }
+
+    public enum UpdateType
+    {
+        ChatTitleChanged,
+        ChatMemberLeft,
+        ChatMembersAdded
+    }
+}
