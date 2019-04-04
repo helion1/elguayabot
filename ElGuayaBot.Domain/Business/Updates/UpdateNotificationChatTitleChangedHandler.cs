858899c47d40520b8516e3ac385e3d7517688c4e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-04 13:19:45 +0200

    Added trace & information logging for ChatTitleChanged

--- ElGuayaBot.Domain/Business/Updates/UpdateNotificationChatTitleChangedHandler.cs
+++ ElGuayaBot.Domain/Business/Updates/UpdateNotificationChatTitleChangedHandler.cs
@@ -1,21 +1,24 @@
 using System.Threading.Tasks;
 using ElGuayaBot.Persistence.Contract;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace ElGuayaBot.Domain.Business.Updates
 {
     public class UpdateNotificationChatTitleChangedHandler : NotificationHandler<UpdateNotification>
     {
+        private readonly Logger<UpdateNotificationChatTitleChangedHandler> Logger;
         private readonly IUnitOfWork _unitOfWork;
-
-        public UpdateNotificationChatTitleChangedHandler(IUnitOfWork unitOfWork)
+        public UpdateNotificationChatTitleChangedHandler(Logger<UpdateNotificationChatTitleChangedHandler> logger, IUnitOfWork unitOfWork)
         {
+            Logger = logger;
             _unitOfWork = unitOfWork;
         }
 
         protected override async void Handle(UpdateNotification notification)
         {
             if (notification.Type != UpdateType.ChatTitleChanged) return;
+            Logger.LogTrace($"ChatTitleChanged update handler triggered.");
 
             var chat = await _unitOfWork.ChatRepository.FindById(notification.ChatId);
 
@@ -24,6 +27,9 @@ namespace ElGuayaBot.Domain.Business.Updates
             _unitOfWork.ChatRepository.Update(chat);
 
             await _unitOfWork.SaveAsync();
+            
+            Logger.LogInformation($"Chat title for chat ({chat.Id}) has been changed to '{chat.Title}'.");
+
         }
     }
 }
