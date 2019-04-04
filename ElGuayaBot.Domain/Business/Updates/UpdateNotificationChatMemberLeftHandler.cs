947396487b97a8ef80d826dd1714cd3f365d3099
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-04 13:19:15 +0200

    Added trace logging for ChatMemberLeft

--- ElGuayaBot.Domain/Business/Updates/UpdateNotificationChatMemberLeftHandler.cs
+++ ElGuayaBot.Domain/Business/Updates/UpdateNotificationChatMemberLeftHandler.cs
@@ -1,23 +1,28 @@
 using ElGuayaBot.Domain.Business.ChatsUsers.DeleteUserFromChat;
-using ElGuayaBot.Domain.Business.Messages;
+using ElGuayaBot.Domain.Business.Requests;
 using ElGuayaBot.Domain.Entity;
 using ElGuayaBot.Persistence.Contract;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace ElGuayaBot.Domain.Business.Updates
 {
     public class UpdateNotificationChatMemberLeftHandler :  NotificationHandler<UpdateNotification>
     {
+        private readonly Logger<UpdateNotificationChatMemberLeftHandler> Logger;
         private readonly IMediator _mediatR;
-        public UpdateNotificationChatMemberLeftHandler(IMediator mediatR)
+
+        public UpdateNotificationChatMemberLeftHandler(Logger<UpdateNotificationChatMemberLeftHandler> logger, IMediator mediatR)
         {
+            Logger = logger;
             _mediatR = mediatR;
         }
 
         protected override void Handle(UpdateNotification notification)
         {
             if (notification.Type != UpdateType.ChatMemberLeft) return;
-            
+            Logger.LogTrace($"ChatMemberLeft update handler triggered.");
+
             var leftUser = notification.LeftChatMember;
 
             if (leftUser.IsBot) return;
