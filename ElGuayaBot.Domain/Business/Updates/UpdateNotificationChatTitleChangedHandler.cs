ac1176b5bc8629cdba88f1b5fb6026e0c9cd9411
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-04 12:47:37 +0200

    Implemented handler for ChatTitleChanged

--- ElGuayaBot.Domain/Business/Updates/UpdateNotificationChatTitleChangedHandler.cs
+++ ElGuayaBot.Domain/Business/Updates/UpdateNotificationChatTitleChangedHandler.cs
@@ -1,7 +1,29 @@
+using System.Threading.Tasks;
+using ElGuayaBot.Persistence.Contract;
+using MediatR;
+
 namespace ElGuayaBot.Domain.Business.Updates
 {
-    public class UpdateNotificationChatTitleChangedHandler
+    public class UpdateNotificationChatTitleChangedHandler : NotificationHandler<UpdateNotification>
     {
-        
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateNotificationChatTitleChangedHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        protected override async void Handle(UpdateNotification notification)
+        {
+            if (notification.Type != UpdateType.ChatTitleChanged) return;
+
+            var chat = await _unitOfWork.ChatRepository.FindById(notification.ChatId);
+
+            chat.Title = notification.NewChatTitle;
+
+            _unitOfWork.ChatRepository.Update(chat);
+
+            await _unitOfWork.SaveAsync();
+        }
     }
 }
