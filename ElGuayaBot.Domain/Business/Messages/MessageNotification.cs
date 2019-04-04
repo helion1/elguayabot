1e41f2f57aa43fd8288bd32c21bb1544ab2d0bc4
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-04 16:14:29 +0200

    Created Message Notification

---
+++ ElGuayaBot.Domain/Business/Messages/MessageNotification.cs
@@ -0,0 +1,29 @@
+using ElGuayaBot.Domain.Entity;
+
+namespace ElGuayaBot.Domain.Business.Messages
+{
+    public class MessageNotification
+    {
+        public int Id { get; set; }
+        public MessageType Type { get; set; }
+        public Chat Chat { get; set; }
+        public User From { get; set; }
+        public string Text { get; set; }
+        public string Command { get; set; }
+        public string Url { get; set; }
+        public string[] Mentions { get; set; }
+    }
+
+    public enum MessageType
+    {  
+        Other,
+        Text,
+        Photo,
+        Audio,
+        Video,
+        Voice,
+        Document,
+        Sticker,
+        Location
+    }
+}
