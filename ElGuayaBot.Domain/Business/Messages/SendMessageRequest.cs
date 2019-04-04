ca87d97bb821d30acd5a775ffea038ba781924e6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-04 10:43:26 +0200

    Updated messagerequest

--- ElGuayaBot.Domain/Business/Messages/SendMessageRequest.cs
+++ ElGuayaBot.Domain/Business/Messages/SendMessageRequest.cs
@@ -5,7 +5,6 @@ namespace ElGuayaBot.Domain.Business.Messages
     public class SendMessageRequest : INotification
     {
         public long ChatId { get; set; }
-        public long ReplyToMessageId { get; set; }
         public string Message { get; set; }
     }
 }
