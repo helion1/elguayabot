e3c55c9440ab831c7f2feab4e17d57c2e2cf791b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-04-05 12:45:24 +0200

    Implemented handler for ChangeChatTitle command

---
+++ ElGuayaBot.Domain/Business/Chats/ChangeChatTitle/ChangeChatTitleCommandHandler.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ElGuayaBot.Common.Result;
+using ElGuayaBot.Persistence.Contract;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayaBot.Domain.Business.Chats.ChangeChatTitle
+{
+    public class ChangeChatTitleCommandHandler : Common.Request.RequestHandler<ChangeChatTitleCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        
+        public ChangeChatTitleCommandHandler(ILogger<Common.Request.RequestHandler<ChangeChatTitleCommand, Result>> logger, IMediator mediatR, IUnitOfWork unitOfWork) : base(logger, mediatR)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public override async Task<Result> Handle(ChangeChatTitleCommand request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var chat = await _unitOfWork.ChatRepository.FindById(request.ChatId);
+
+                chat.Title = request.NewChatTitle;
+
+                _unitOfWork.ChatRepository.Update(chat);
+
+                await _unitOfWork.SaveAsync();
+            
+                Logger.LogInformation($"Chat title for chat ({chat.Id}) has been changed to '{chat.Title}'.");
+                
+                return Result.Success();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError("Handler for 'ChangeChatTitleCommand' encountered an unknown error", e);
+            }
+            
+            return Result.Success();
+        }
+    }
+}
