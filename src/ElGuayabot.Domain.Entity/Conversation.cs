3108f14fc0ed22131913a94a14039494638a9f18
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-16 14:20:48 +0200

    Added ChatId and UserId to Conversation entity

--- src/ElGuayabot.Domain.Entity/Conversation.cs
+++ src/ElGuayabot.Domain.Entity/Conversation.cs
@@ -1,8 +1,11 @@
 namespace ElGuayabot.Domain.Entity
 {
     public class Conversation
-    {        
-          public Chat Chat { get; set; }
-          public User User { get; set; }
+    {
+        public long ChatId { get; set; }
+        public Chat Chat { get; set; }
+        
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
