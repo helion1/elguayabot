1e0b274b183c94dcf432cfc0221a7701457251f0
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-17 17:48:32 +0200

    Implemented IConversationRepository

---
+++ src/ElGuayabot.Persistence.Implementation/Repository/ConversationRepository.cs
@@ -0,0 +1,14 @@
+﻿using ElGuayabot.Domain.Entity;
+using ElGuayabot.Persistence.Contract.Repository;
+using ElGuayabot.Persistence.Implementation.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElGuayabot.Persistence.Implementation.Repository
+{
+    public class ConversationRepository : GenericRepository<Conversation>, IConversationRepository
+    {
+        public ConversationRepository(ElGuayabotDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
