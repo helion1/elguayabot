0c1e5446da62675eb98d33beacebad047ea40d96
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-17 17:30:03 +0200

    Erased unused handlers of telegrame events

--- src/ElGuayabot.Application.Implementation/Service/TelegramService.cs
+++ src/ElGuayabot.Application.Implementation/Service/TelegramService.cs
@@ -28,8 +28,6 @@ namespace ElGuayabot.Application.Implementation.Service
         public void StartBot()
         {
             Bot.Client.OnMessage += HandleOnMessage;
-            Bot.Client.OnCallbackQuery += HandleOnCallbackQuery;
-            Bot.Client.OnInlineQuery += HandleOnInlineQuery;
 
             Bot.Start();
 
@@ -91,75 +89,6 @@ namespace ElGuayabot.Application.Implementation.Service
             catch (Exception exception)
             {
                 Logger.LogError(exception, "Unhandled error processing message ({@Message}).", e.Message);
-            }
-        }
-
-        private async void HandleOnCallbackQuery(object sender, CallbackQueryEventArgs e)
-        {
-            try
-            {
-                using (var scope = ServiceScopeFactory.CreateScope())
-                {
-                    var botContext = scope.ServiceProvider.GetRequiredService<IBotContext>();
-                    botContext.Populate(e.CallbackQuery);
-
-                    var mediatR = scope.ServiceProvider.GetRequiredService<IMediator>();
-                    var actionSelector = scope.ServiceProvider.GetRequiredService<IBotActionSelector>();
-
-                    var callbackActionResult = actionSelector.GetCallbackAction();
-
-                    if (!callbackActionResult.Succeeded) return;
-
-                    var requestResult = await mediatR.Send(callbackActionResult.Value);
-
-                    if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
-                    {
-                        Logger.LogError("{CallbackQueryAction} was not processed correctly: {@Errors}",
-                            callbackActionResult.Value.GetType().Name, requestResult.Errors);
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                Logger.LogError(exception, "Unhandled error processing callback query ({@CallbackQuery}).", e.CallbackQuery);
-            }
-            finally
-            {
-#pragma warning disable 4014
-                Bot.Client.AnswerCallbackQueryAsync(e.CallbackQuery.Id);
-#pragma warning restore 4014
-            }
-        }
-
-        private async void HandleOnInlineQuery(object sender, InlineQueryEventArgs e)
-        {
-            try
-            {
-                using (var scope = ServiceScopeFactory.CreateScope())
-                {
-                    var botContext = scope.ServiceProvider.GetRequiredService<IBotContext>();
-                    botContext.Populate(e.InlineQuery);
-
-                    var mediatR = scope.ServiceProvider.GetRequiredService<IMediator>();
-                    var actionSelector = scope.ServiceProvider.GetRequiredService<IBotActionSelector>();
-
-
-                    var inlineActionResult = actionSelector.GetInlineAction();
-
-                    if (!inlineActionResult.Succeeded) return;
-
-                    var requestResult = await mediatR.Send(inlineActionResult.Value);
-
-                    if (!requestResult.Succeeded && !requestResult.Errors.ContainsKey("not_found"))
-                    {
-                        Logger.LogError("{InlineQueryAction} request was not processed correctly: {@Errors}",
-                            inlineActionResult.Value.GetType().Name, requestResult.Errors);
-                    }
-                }
-            }
-            catch (Exception exception)
-            {
-                Logger.LogError(exception, "Unhandled error processing inline query ({@InlineQuery}).", e.InlineQuery);
             }
         }
     }
