f5e20cf48090dbd809d0dda4e87d59d00e23a145
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-17 13:28:07 +0200

    Updated User summaries

--- src/ElGuayabot.Domain.Entity/User.cs
+++ src/ElGuayabot.Domain.Entity/User.cs
@@ -5,10 +5,25 @@ namespace ElGuayabot.Domain.Entity
 {
     public class User
     {
+        /// <summary>
+        /// Id of the user.
+        /// </summary>
         public int Id { get; set; }
+        /// <summary>
+        /// Specifies if the user is a bot or no.
+        /// </summary>
         public bool IsBot { get; set; }
+        /// <summary>
+        /// Language code of the user client.
+        /// </summary>
         public string LanguageCode { get; set; }
+        /// <summary>
+        /// Username of the user.
+        /// </summary>
         public string Username { get; set; }
+        /// <summary>
+        /// Date of first interaction with the user.
+        /// </summary>
         public DateTime FirstSeen { get; set; }
         
         public List<Conversation> Conversations { get; set; }
