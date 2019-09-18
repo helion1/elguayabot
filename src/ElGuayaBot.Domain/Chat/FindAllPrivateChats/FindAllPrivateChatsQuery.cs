127bd0b5a7b0983508934dae5d2bd8c9335650b8
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 16:21:59 +0200

    Implemented FindAllPrivateChats query

---
+++ src/ElGuayaBot.Domain/Chat/FindAllPrivateChats/FindAllPrivateChatsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+
+namespace ElGuayabot.Domain.Chat.FindAllPrivateChats
+{
+    public class FindAllPrivateChatsQuery : Request<Result<IEnumerable<Entity.Chat>>>
+    {
+        
+    }
+}
