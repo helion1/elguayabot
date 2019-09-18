c66a54e73d0e5aba8828c4ef55df0e388853f5c9
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 15:20:32 +0200

    Created MiscellaneousAction

---
+++ src/ElGuayabot.Application.Contract/Model/Action/Miscellaneous/MiscellaneousAction.cs
@@ -0,0 +1,11 @@
+﻿using ElGuayabot.Application.Contract.Common.Context;
+
+namespace ElGuayabot.Application.Contract.Model.Action.Miscellaneous
+{
+    public abstract class MiscellaneousAction : BotAction, IMiscellaneousAction
+    {
+        public MiscellaneousAction(IBotContext botContext) : base(botContext)
+        {
+        }
+    }
+}
