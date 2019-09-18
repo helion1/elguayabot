a525f1c7dec4bba1a428e857e4ea15f84233ca76
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 17:14:29 +0200

    Erassed unused code/imports

--- src/ElGuayabot.Application.Implementation/OldLogic/Command/AboutLogic/AboutRequest.cs
+++
@@ -1,9 +0,0 @@
-using ElGuayabot.Application.Implementation.Logic.Common.AbstractLogic;
-
-namespace ElGuayabot.Application.Implementation.Logic.Command.AboutLogic
-{
-    public class AboutRequest : AbstractMessageRequest
-    {
-        
-    }
-}
