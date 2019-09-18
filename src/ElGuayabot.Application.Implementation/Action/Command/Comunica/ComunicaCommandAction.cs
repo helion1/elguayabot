98f780a2847af4984ede72e0238a6a2aefe96818
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 15:07:59 +0200

    Created 'comunica' command action

---
+++ src/ElGuayabot.Application.Implementation/Action/Command/Comunica/ComunicaCommandAction.cs
@@ -0,0 +1,17 @@
+﻿using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Application.Contract.Model.Action.Command;
+
+namespace ElGuayabot.Application.Implementation.Action.Command.Comunica
+{
+    public class ComunicaCommandAction : CommandAction
+    {
+        public ComunicaCommandAction(IBotContext botContext) : base(botContext)
+        {
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return condition == "/comunica";
+        }
+    }
+}
