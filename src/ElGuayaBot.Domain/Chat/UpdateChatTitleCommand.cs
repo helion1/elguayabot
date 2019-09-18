d09a4969141023727f48704724565167e58764b5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 12:45:03 +0200

    Created command for updating chat title

---
+++ src/ElGuayaBot.Domain/Chat/UpdateChatTitleCommand.cs
@@ -0,0 +1,17 @@
+﻿using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+
+namespace ElGuayabot.Domain.Chat
+{
+    public class UpdateChatTitleCommand : Request<Result>
+    {
+        /// <summary>
+        /// Id of the chat.
+        /// </summary>
+        public long ChatId { get; set; }
+        /// <summary>
+        /// New title of the chat.
+        /// </summary>
+        public string Title { get; set; }
+    }
+}
