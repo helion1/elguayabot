58b4ad991ffa9343027feb97b5649d5c606a2c8f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 15:08:11 +0200

    Created 'echo' command action

---
+++ src/ElGuayabot.Application.Implementation/Action/Command/Echo/EchoCommandAction.cs
@@ -0,0 +1,17 @@
+﻿using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Application.Contract.Model.Action.Command;
+
+namespace ElGuayabot.Application.Implementation.Action.Command.Echo
+{
+    public class EchoCommandAction : CommandAction
+    {
+        public EchoCommandAction(IBotContext botContext) : base(botContext)
+        {
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return condition == "/echo";
+        }
+    }
+}
