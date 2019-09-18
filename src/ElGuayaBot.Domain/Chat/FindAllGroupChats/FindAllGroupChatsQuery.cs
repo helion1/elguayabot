3dc1c8345e4cad282f34cd1dc0b94e698cc99e3b
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-18 16:15:53 +0200

    Implemented FindAllGroupChatsQuery

---
+++ src/ElGuayaBot.Domain/Chat/FindAllGroupChats/FindAllGroupChatsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+
+namespace ElGuayabot.Domain.Chat.FindAllGroupChats
+{
+    public class FindAllGroupChatsQuery : Request<Result<IEnumerable<Entity.Chat>>>
+    {
+        
+    }
+}
---
+++ src/ElGuayaBot.Domain/Chat/FindAllGroupChats/FindAllGroupChatsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+using ElGuayabot.Persistence.Contract;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayabot.Domain.Chat.FindAllGroupChats
+{
+    public class FindAllGroupChatsQueryHandler: CommonHandler<FindAllGroupChatsQuery, Result<IEnumerable<Entity.Chat>>>
+    {
+        protected readonly IUnitOfWork UnitOfWork;
+
+        public FindAllGroupChatsQueryHandler(ILogger<CommonHandler<FindAllGroupChatsQuery, Result<IEnumerable<Entity.Chat>>>> logger, IMediator mediatR, IUnitOfWork unitOfWork) : base(logger, mediatR)
+        {
+            UnitOfWork = unitOfWork;
+        }
+
+        public override async Task<Result<IEnumerable<Entity.Chat>>> Handle(FindAllGroupChatsQuery request, CancellationToken cancellationToken)
+        {
+            var groupChats = await UnitOfWork.ChatRepository.SearchBy(c => c.Type == Entity.Chat.ChatType.Group);
+            
+            return  Result<IEnumerable<Entity.Chat>>.Success(groupChats);
+        }
+    }
+}
