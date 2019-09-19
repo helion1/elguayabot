58abd53c1c6b84289aa77399839d8622d73700c5
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 17:12:31 +0200

    Implemented "CallateLaJeta"

---
+++ src/ElGuayabot.Application.Implementation/Action/Miscellaneous/CallateLaJeta/CallateLaJetaMiscellaneousAction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Application.Contract.Model.Action.Miscellaneous;
+using ElGuayabot.Application.Implementation.Common.Context;
+using ElGuayabot.Common.Request;
+using NeoSmart.Unicode;
+
+namespace ElGuayabot.Application.Implementation.Action.Miscellaneous.CallateLaJeta
+{
+    public class CallateLaJetaMiscellaneousAction : MiscellaneousAction
+    {
+        public DateTime TimeStamp { get; set; }
+        
+        public CallateLaJetaMiscellaneousAction(IBotContext botContext) : base(botContext)
+        {
+            TimeStamp = botContext.Message.Date;
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return IsAllUpper(condition) 
+                    && condition != "OMG" 
+                    && condition != "OMFG" 
+                    && condition != "LMAO" 
+                    && condition != "F" 
+                    && condition != "LMAOO" 
+                    && condition != "WTF" 
+                    && condition != "LOL"
+                    && TimeStamp.Millisecond % 2 == 0;
+        }
+        
+        private bool IsAllUpper(string input)
+        {
+            var containsLetters = false;
+            if (Emoji.IsEmoji((input)))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]))
+                {
+                    containsLetters = true;
+                }
+            }
+            
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLetter(input[i]) && !Char.IsUpper(input[i]))
+                    return false;
+            }
+            
+            return containsLetters;
+        }
+    }
+}
---
+++ src/ElGuayabot.Application.Implementation/Action/Miscellaneous/CallateLaJeta/CallateLaJetaMiscellaneousActionHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ElGuayabot.Application.Implementation.Common.Response.Text;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Telegram.Bot.Types.Enums;
+
+namespace ElGuayabot.Application.Implementation.Action.Miscellaneous.CallateLaJeta
+{
+    public class CallateLaJetaMiscellaneousActionHandler : CommonHandler<CallateLaJetaMiscellaneousAction, Result>
+    {
+        public CallateLaJetaMiscellaneousActionHandler(ILogger<CommonHandler<CallateLaJetaMiscellaneousAction, Result>> logger, IMediator mediatR) : base(logger, mediatR)
+        {
+        }
+
+        public override async Task<Result> Handle(CallateLaJetaMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            var responses = new[]
+            {
+                "CÁLLATE LA JETA MALDITO SAPO",
+                "NO GRITES JODER",
+                "VERGA CALLATE YA"
+            };
+
+            var rnd = new Random();
+            
+            var r = rnd.Next(responses.Length);
+            
+            var response = $"<b>{responses[r]}</b>";
+
+            return await MediatR.Send(new TextResponse(response, ParseMode.Html), cancellationToken);
+        }
+    }
+}
