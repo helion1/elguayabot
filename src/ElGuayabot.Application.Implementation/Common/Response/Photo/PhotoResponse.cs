e6fc1053fcc52664365944f7b74ff66e877a2e1e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 11:10:39 +0200

    Implemented PhotoResponse

---
+++ src/ElGuayabot.Application.Implementation/Common/Response/Photo/PhotoResponse.cs
@@ -0,0 +1,16 @@
+﻿using ElGuayabot.Application.Contract.Common.Responses;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+
+namespace ElGuayabot.Application.Implementation.Common.Response.Photo
+{
+    public class PhotoResponse : Request<Result>, IResponse
+    {
+        public string Photo { get; set; }
+
+        public PhotoResponse(string photo)
+        {
+            Photo = photo;
+        }
+    }
+}
---
+++ src/ElGuayabot.Application.Implementation/Common/Response/Photo/PhotoResponseHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayabot.Application.Implementation.Common.Response.Photo
+{
+    public class PhotoResponseHandler : CommonHandler<PhotoResponse, Result>
+    {
+        protected readonly IBotContext BotContext;
+        
+        public PhotoResponseHandler(ILogger<CommonHandler<PhotoResponse, Result>> logger, IMediator mediatR, IBotContext botContext) : base(logger, mediatR)
+        {
+            BotContext = botContext;
+        }
+
+        public override async Task<Result> Handle(PhotoResponse request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await BotContext.BotClient.Client.SendPhotoAsync(BotContext.Chat.Id, request.Photo, cancellationToken: cancellationToken);
+
+                return Result.Success();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error sending photo ({@Request}).", request);
+                
+                return Result.UnknownError(new List<string> {e.Message});
+            }
+        }
+    }
+}
