82d3143ba5cef55cb8b6167796b1a67b58a9c0f7
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 11:49:03 +0200

    Implemented StickerResponse

---
+++ src/ElGuayabot.Application.Implementation/Common/Response/Sticker/StickerResponse.cs
@@ -0,0 +1,16 @@
+﻿using ElGuayabot.Application.Contract.Common.Responses;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+
+namespace ElGuayabot.Application.Implementation.Common.Response.Sticker
+{
+    public class StickerResponse : Request<Result>, IResponse
+    {
+        public string Sticker { get; set; }
+
+        public StickerResponse(string sticker)
+        {
+            Sticker = sticker;
+        }
+    }
+}
---
+++ src/ElGuayabot.Application.Implementation/Common/Response/Sticker/StickerResponseHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayabot.Application.Implementation.Common.Response.Sticker
+{
+    public class StickerResponseHandler : CommonHandler<StickerResponse, Result>
+    {
+        protected readonly IBotContext BotContext;
+
+        public StickerResponseHandler(ILogger<CommonHandler<StickerResponse, Result>> logger, IMediator mediatR, IBotContext botContext) : base(logger, mediatR)
+        {
+            BotContext = botContext;
+        }
+
+        public override async Task<Result> Handle(StickerResponse request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await BotContext.BotClient.Client.SendStickerAsync(BotContext.Chat.Id, request.Sticker, cancellationToken: cancellationToken);
+
+                return Result.Success();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, "Unhandled error sending text response ({@Request}).", request);
+                
+                return Result.UnknownError(new List<string> {e.Message});
+            }
+        }
+    }
+}
