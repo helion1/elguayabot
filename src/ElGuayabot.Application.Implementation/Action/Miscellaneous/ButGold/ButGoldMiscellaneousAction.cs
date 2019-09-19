c7b043ff869e121937a4b078513e53d915a0b377
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 16:56:06 +0200

    Implemented "but gold"

---
+++ src/ElGuayabot.Application.Implementation/Action/Miscellaneous/ButGold/ButGoldMiscellaneousAction.cs
@@ -0,0 +1,17 @@
+﻿using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Application.Contract.Model.Action.Miscellaneous;
+
+namespace ElGuayabot.Application.Implementation.Action.Miscellaneous.ButGold
+{
+    public class ButGoldMiscellaneousAction : MiscellaneousAction
+    {
+        public ButGoldMiscellaneousAction(IBotContext botContext) : base(botContext)
+        {
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return condition.ToLower().Contains("old");
+        }
+    }
+}
