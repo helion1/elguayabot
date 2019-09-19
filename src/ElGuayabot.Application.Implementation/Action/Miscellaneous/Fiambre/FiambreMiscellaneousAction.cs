f2680f2f4b9837c57daad13c71c3e5c91812051c
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 17:23:27 +0200

    Implemented Fiambre

---
+++ src/ElGuayabot.Application.Implementation/Action/Miscellaneous/Fiambre/FiambreMiscellaneousAction.cs
@@ -0,0 +1,17 @@
+﻿using ElGuayabot.Application.Contract.Common.Context;
+using ElGuayabot.Application.Contract.Model.Action.Miscellaneous;
+
+namespace ElGuayabot.Application.Implementation.Action.Miscellaneous.Fiambre
+{
+    public class FiambreMiscellaneousAction : MiscellaneousAction
+    {
+        public FiambreMiscellaneousAction(IBotContext botContext) : base(botContext)
+        {
+        }
+
+        public override bool CanHandle(string condition)
+        {
+            return (condition.ToLower().Contains("tengo hambre") && !condition.ToLower().Contains("no tengo hambre"));
+        }
+    }
+}
---
+++ src/ElGuayabot.Application.Implementation/Action/Miscellaneous/Fiambre/FiambreMiscellaneousActionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ElGuayabot.Application.Implementation.Common.Response.Text;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayabot.Application.Implementation.Action.Miscellaneous.Fiambre
+{
+    public class FiambreMiscellaneousActionHandler : CommonHandler<FiambreMiscellaneousAction, Result>
+    {
+        public FiambreMiscellaneousActionHandler(ILogger<CommonHandler<FiambreMiscellaneousAction, Result>> logger, IMediator mediatR) : base(logger, mediatR)
+        {
+        }
+
+        public override async Task<Result> Handle(FiambreMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            return await MediatR.Send(new TextResponse("pues aquí hay fiambre JAJAJAJAJAJA")); //TODO: reply
+        }
+    }
+}
