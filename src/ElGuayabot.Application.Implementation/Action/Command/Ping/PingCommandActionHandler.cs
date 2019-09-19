307a1b716d4044b7148a22d1de19f6b19fc7435e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 19:51:12 +0200

    Added some insults

--- src/ElGuayabot.Application.Implementation/Action/Command/Ping/PingCommandActionHandler.cs
+++ src/ElGuayabot.Application.Implementation/Action/Command/Ping/PingCommandActionHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using ElGuayabot.Application.Implementation.Common.Response.Text;
 using ElGuayabot.Common.Request;
@@ -18,7 +19,23 @@ namespace ElGuayabot.Application.Implementation.Action.Command.Ping
         {
             var difference = request.Timestamp - request.MessageSentDateTime;
 
-            var text =$"_pong!_ He tardado `{difference.TotalSeconds:N2}` milisegundos en recibir _tu_ mensaje. Los mismos que duras tu en la cama, *muerdealmohadas*";
+            var insults = new[]
+            {
+                "muerdealmohadas",
+                "comepingas",
+                "jalabolas",
+                "pendejo",
+                "mamaguebo",
+                "becerro"
+            };
+            
+            var rnd = new Random();
+
+            var n = rnd.Next(insults.Length);
+
+            var luckyInsult = insults[n];
+            
+            var text =$"_pong!_ He tardado `{difference.TotalSeconds:N2}` milisegundos en recibir _tu_ mensaje. Los mismos que duras tu en la cama, *{luckyInsult}*";
 
             return await MediatR.Send(new TextResponse(text), cancellationToken);
         }
