250909a698bb0ffe2ca22ed5fe5160f4a4fb1278
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-19 17:17:01 +0200

    Implemented ColdMeat

---
+++ src/ElGuayabot.Application.Implementation/Action/Miscellaneous/ColdMeat/ColdMeatMiscellaneousActionHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ElGuayabot.Application.Implementation.Common.Response.Text;
+using ElGuayabot.Common.Request;
+using ElGuayabot.Common.Result;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayabot.Application.Implementation.Action.Miscellaneous.ColdMeat
+{
+    public class ColdMeatMiscellaneousActionHandler : CommonHandler<ColdMeatMiscellaneousAction, Result>
+    {
+        public ColdMeatMiscellaneousActionHandler(ILogger<CommonHandler<ColdMeatMiscellaneousAction, Result>> logger, IMediator mediatR) : base(logger, mediatR)
+        {
+        }
+
+        public override async Task<Result> Handle(ColdMeatMiscellaneousAction request, CancellationToken cancellationToken)
+        {
+            return await MediatR.Send(new TextResponse("well here is cold meat.")); //TODO: reply
+        }
+    }
+}
