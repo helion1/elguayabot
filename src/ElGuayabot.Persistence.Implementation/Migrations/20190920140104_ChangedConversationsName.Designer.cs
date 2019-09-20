1991f40418743f768bfce4d175290aa58ce8caaf
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-20 16:47:52 +0200

    Updated migrations

--- src/ElGuayabot.Persistence.Implementation/Migrations/20190920140104_ChangedConversationsName.Designer.cs
+++ src/ElGuayabot.Persistence.Implementation/Migrations/20190920140104_ChangedConversationsName.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using ElGuayabot.Persistence.Implementation.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace ElGuayabot.Persistence.Implementation.Migrations
 {
     [DbContext(typeof(ElGuayabotDbContext))]
-    partial class ElGuayabotDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190920140104_ChangedConversationsName")]
+    partial class ChangedConversationsName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
