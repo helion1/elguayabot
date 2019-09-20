c81e69e67838682b1021d8f352dffb050f788afc
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-20 09:28:53 +0200

    Created Other action

---
+++ src/ElGuayabot.Application.Implementation/Action/Command/Other/OtherCommandAction.cs
@@ -0,0 +1,12 @@
+﻿namespace ElGuayabot.Application.Implementation.Action.Command.Other
+{
+    public class OtherCommandAction
+    {
+        //            var responses = new List<string>
+        //            {
+        //                "No te entiendo maldito lázaro",
+        //                "Sacate el huevo de la boca"
+        //            };
+        //            
+    }
+}
