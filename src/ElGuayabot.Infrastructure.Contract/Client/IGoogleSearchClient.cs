a6282826804e2e225afa7921a6e72d6f2373a561
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-09-20 09:46:34 +0200

    Implemented GoogleSearchClient

---
+++ src/ElGuayabot.Infrastructure.Contract/Client/IGoogleSearchClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ElGuayabot.Common.Result;
+
+namespace ElGuayabot.Infrastructure.Contract.Client
+{
+    public interface IGoogleSearchClient
+    {
+        Task<Result<string>> Search(params string[] searchParams);
+    }
+}
---
+++ src/ElGuayabot.Infrastructure.Implementation/Client/GoogleSearchClient.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using ElGuayabot.Common.Result;
+using ElGuayabot.Infrastructure.Contract.Client;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ElGuayabot.Infrastructure.Implementation.Client
+{
+    public class GoogleSearchClient : IGoogleSearchClient
+    {
+        protected readonly ILogger<GoogleSearchClient> Logger;
+        protected readonly HttpClient Client;
+        protected readonly string API_KEY;
+
+        public GoogleSearchClient(ILogger<GoogleSearchClient> logger, HttpClient client,  IConfiguration configuration)
+        {
+            client.BaseAddress = new System.Uri(configuration["GOOGLE_URL"] ?? "https://www.google.com/");
+            
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/html, application/xhtml+xml, */*"));
+            
+            client.DefaultRequestHeaders.UserAgent.Clear();
+            client.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"));
+            
+            Logger = logger;
+            Client = client;
+        }
+        
+        public async Task<Result<string>> Search(params string[] searchParams)
+        {
+            var searchQuery = string.Join('+', searchParams);
+
+            var searchResponse = await Client.GetAsync($"search?q={searchQuery}&tbm=isch");
+
+            if (!searchResponse.IsSuccessStatusCode)
+            {
+                return Result<string>.Fail(searchResponse.StatusCode.ToString(), new List<string> {searchResponse.ReasonPhrase});
+            }
+
+            var content = await searchResponse.Content.ReadAsStringAsync();
+            
+            return Result<string>.Success(content);
+        }
+    }
+}
