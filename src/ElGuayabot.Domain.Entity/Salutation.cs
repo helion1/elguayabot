e8e7c0ba0a6dfdcac6e4f1c93334d419fb366b57
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-10-21 15:04:58 +0200

    Created Salutation entity

---
+++ src/ElGuayabot.Domain.Entity/Salutation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ElGuayabot.Domain.Entity
+{
+    public class Salutation
+    {
+        public Guid Id { get; set; }
+        public string Message { get; set; }
+        
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
